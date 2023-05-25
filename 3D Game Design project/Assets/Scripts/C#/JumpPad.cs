f890a7134becae2d0381780a68751b89b53aa61f
YellowySmile979 <xcant5@gmail.com>
2023-05-25 12:52:01 +0800

    jump pad cont

--- 3D Game Design project/Assets/Scripts/C#/JumpPad.cs
+++ 3D Game Design project/Assets/Scripts/C#/JumpPad.cs
@@ -6,36 +6,64 @@ public class JumpPad : MonoBehaviour
 {
     public Transform destination;
     public float speed = 7f;
+    public bool disableDestinationJumpPad = true;
 
+    //all the objects that we do not want to launch
+    List<Transform> noFlyList = new List<Transform>();
+
+    public void NotifyNoFly(Transform t)
+    {
+        noFlyList.Add(t);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //if we r on no fly list, then we cannot be launched
+        //which prevents us from getting launched when landing on a jump pad
+        if (noFlyList.Contains(other.transform))
+        {
+            noFlyList.Remove(other.transform);
+            return;
+        }
+        //begin launch
+        if (other.GetComponent<CubeRoll>() || other.GetComponent<PushBlock>())
+        {
+            StartCoroutine(Jump(other.transform));
+        }
+    }
     IEnumerator Jump(Transform launchTarget)
     {
         WaitForEndOfFrame w = new WaitForEndOfFrame();
 
+        //notifies the landing jump pad that it should not allow incoming object to be launched again
+        JumpPad jumpPad = destination.GetComponent<JumpPad>();
+        if (jumpPad) jumpPad.NotifyNoFly(launchTarget);
+
+        //before we jump, check if the object has a rigidbody. if it does, set it to Kinematic
+        Rigidbody rb = launchTarget.GetComponent<Rigidbody>();
+        if (rb) rb.isKinematic = true;
+
         //record some of our key variables
-        float dist = 0f;
+        float dist = 0f, totalDist = Vector3.Distance(launchTarget.position, destination.position + Vector3.up);
+
+        //where the launch target will actually be
+        Vector3 actualPosition = launchTarget.position;
+
         //"Update" loop
-        while (Vector2.Distance(
-            new Vector2(launchTarget.position.x, launchTarget.position.z), 
-            new Vector2(destination.position.x, destination.position.z)
-            ) < 0.01f)
+        while (dist < totalDist)
         {
-            //our "Update" loop
-            launchTarget.position = Vector3.MoveTowards(launchTarget.position, destination.position, speed);
-            dist += speed;
+            float delta = speed * Time.deltaTime;
+            actualPosition = Vector3.MoveTowards(actualPosition, 
+                destination.position + Vector3.up, delta);
+            dist += delta;
+
+            //move launch target to the actual position + vertical offset
+            float offsetHeight = totalDist * Mathf.Sin(Mathf.PI * dist / totalDist);
+            launchTarget.position = actualPosition + Vector3.up * offsetHeight;
 
             yield return w;
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        launchTarget.position = destination.position + Vector3.up;
+        //disable kinematic for the rb again
+        if (rb) rb.isKinematic = false;
     }
 }
