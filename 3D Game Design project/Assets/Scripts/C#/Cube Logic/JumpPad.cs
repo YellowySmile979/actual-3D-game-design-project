5ea6b392f077a2a7ee93345e01db38ebefcf5e7d
YellowySmile979 <xcant5@gmail.com>
2023-06-13 13:50:28 +0800

    more bug fixing for ange

--- 3D Game Design project/Assets/Scripts/C#/Cube Logic/JumpPad.cs
+++ 3D Game Design project/Assets/Scripts/C#/Cube Logic/JumpPad.cs
@@ -7,10 +7,24 @@ public class JumpPad : MonoBehaviour
     public Transform destination;
     public float speed = 7f;
     public bool disableDestinationJumpPad = true;
+    public float waitTime = 0.7f;
+    float pushBlockWaitTime;
+    public Vector3 pushBlockPushDirection;
+    GameObject pushBlock;
 
     //all the objects that we do not want to launch
-    List<Transform> noFlyList = new List<Transform>();
+    public List<Transform> noFlyList = new List<Transform>();
 
+    void Start()
+    {
+        //sets the waitTime to always be greater than 0.7f
+        //otherwise the pushblock would wait too short of a time and bug out
+        if(waitTime < 0.7f)
+        {
+            waitTime = 0.7f;           
+        }
+        pushBlockWaitTime = waitTime;
+    }
     public void NotifyNoFly(Transform t)
     {
         noFlyList.Add(t);
@@ -27,12 +41,29 @@ public class JumpPad : MonoBehaviour
         //begin launch
         if (other.GetComponent<CubeRoll>() || other.GetComponent<PushBlock>())
         {
-            StartCoroutine(Jump(other.transform));
+            //checks to see if the collided block is a PushBlock
+            //if so, update the GameObject pushBlock with the info and set the waitTime to the one for pushBlock
+            //otherwise set it to zero to be instantaneous
+            //u could make this a switch case if u want
+            if (other.GetComponent<PushBlock>())
+            {
+                pushBlock = other.gameObject;
+                waitTime = pushBlockWaitTime;
+            }
+            else if (other.GetComponent<CubeRoll>())
+            {
+                waitTime = 0f;
+            }
+            StartCoroutine(Jump(other.transform));            
         }
     }
+    //controls the jumping part of the jumppad
     IEnumerator Jump(Transform launchTarget)
     {
         WaitForEndOfFrame w = new WaitForEndOfFrame();
+
+        //waits so that the pushblock doesnt bug out
+        yield return new WaitForSeconds(waitTime);
 
         //notifies the landing jump pad that it should not allow incoming object to be launched again
         JumpPad jumpPad = destination.GetComponent<JumpPad>();
@@ -63,6 +94,13 @@ public class JumpPad : MonoBehaviour
             yield return w;
         }
         launchTarget.position = destination.position + Vector3.up;
+        //causes pushblock to be pushed in a direction so that the push block aint just occupying the jumppad
+        if (pushBlock != null)
+        {
+            launchTarget.position = destination.position + pushBlockPushDirection;
+            //sets the GameObject to null for the next block to be detected
+            pushBlock = null;
+        }
         //disable kinematic for the rb again
         if (rb) rb.isKinematic = false;
     }
